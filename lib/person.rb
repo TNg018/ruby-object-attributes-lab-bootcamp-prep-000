d8bfbc13e058c02231615821e8e0cb977992172c
Tien Nguyen <pri859@sudomail.com>
2018-06-18 16:47:55 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,11 +1,18 @@
 class Person
-  def initialize(name)
+
+  def name=(name)
     @name = name
   end
+
   def name
     @name
   end
-  def job
-    @job = job
+
+  def breed=(breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
   end
 end
